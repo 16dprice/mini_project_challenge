4e96264b40657be0e5e7a043de0a2a49b3f8e2f6
AnonymousWalker <hoanganhtran1998@gmail.com>
2021-01-05 16:33:43 -0500

    add model + migrate

--- backend/web_api/appmodule/models.py
+++ backend/web_api/appmodule/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.TextField()
+    firstname = models.TextField()
+    lastname = models.TextField()
+
+class Language(models.Model):
+    slug = models.CharField(max_length=30, primary_key=True)
+    original_name = models.TextField()
+    anglicized_name = models.TextField()
+
+class Book(models.Model):
+    slug = models.CharField(max_length=10, primary_key=True)
+    name = models.TextField()
+    index = models.IntegerField()
+
+class Project(models.Model):
+    completed = models.BooleanField(default=False)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+    contributors = models.ManyToManyField(User)
