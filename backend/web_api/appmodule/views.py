77730ede46f96115374d368260840047a551cc6e
AnonymousWalker <hoanganhtran1998@gmail.com>
2021-01-07 16:04:33 -0500

    api endpoints getting a project

--- backend/web_api/appmodule/views.py
+++ backend/web_api/appmodule/views.py
@@ -3,16 +3,26 @@ from rest_framework import viewsets
 from .models import Project
 from .serializers import ProjectSerializer
 from rest_framework.response import Response
+from .models import Book, Language
+import logging
 
 # Create your API controllers here.
-
-# class ProjectViewSet(viewsets.ModelViewSet):
-#     projects = Project.objects.all().order_by('id')
-#     serializer = ProjectSerializer(projects, many=True)
-#     return Response(serializer.data)
 
 @api_view(['GET'])
 def get_projects(request):
     projects = Project.objects.all().order_by('id')
     serializer = ProjectSerializer(projects, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_project(request, id):
+    project = Project.objects.filter(id=id).first()
+    serializer = ProjectSerializer(project)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def create_project(request):
+    # TODO: create a project and return the new project info
+    pass
