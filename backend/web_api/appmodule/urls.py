0211d0920c5283eae37a574e8ef8774dac482d35
AnonymousWalker <hoanganhtran1998@gmail.com>
2021-01-07 15:21:17 -0500

    better url pattern, prevent route overriding by accident

--- backend/web_api/appmodule/urls.py
+++ backend/web_api/appmodule/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('projects/', views.get_projects),
+    url(r'projects/?$', views.get_projects),
+    url(r'projects/create/?$', views.create_project),
 ]
