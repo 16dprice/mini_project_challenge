6a8c0eddf758daf85eff34915bf846c612473c49
AnonymousWalker <hoanganhtran1998@gmail.com>
2021-01-07 14:13:11 -0500

    implement first REST endpoint: projects

--- backend/web_api/appmodule/views.py
+++ backend/web_api/appmodule/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework import viewsets
+from .models import Project
+from .serializers import ProjectSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+# Create your API controllers here.
+
+# class ProjectViewSet(viewsets.ModelViewSet):
+#     projects = Project.objects.all().order_by('id')
+#     serializer = ProjectSerializer(projects, many=True)
+#     return Response(serializer.data)
+
+@api_view(['GET'])
+def get_projects(request):
+    projects = Project.objects.all().order_by('id')
+    serializer = ProjectSerializer(projects, many=True)
+    return Response(serializer.data)
