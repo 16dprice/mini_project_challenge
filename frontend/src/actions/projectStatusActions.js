675fe81dd2eb93a451c6c35cfae8c26c25690069
DJ <djpric91@gmail.com>
2021-01-08 08:29:16 -0600

    dispatch actions in projectFilter to redux

---
+++ frontend/src/actions/projectStatusActions.js
@@ -0,0 +1,17 @@
+export const all = () => {
+    return {
+        type: 'ALL'
+    }
+}
+
+export const completed = () => {
+    return {
+        type: 'COMPLETED'
+    }
+}
+
+export const uncompleted = () => {
+    return {
+        type: 'UNCOMPLETED'
+    }
+}
