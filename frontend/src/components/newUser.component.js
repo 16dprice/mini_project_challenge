ff719f6956c48828bef5dac2d26c1cc25887f9a7
DJ <djpric91@gmail.com>
2021-01-09 11:38:56 -0600

    display modal when click on new user button

--- frontend/src/components/newUser.component.js
+++ frontend/src/components/newUser.component.js
@@ -1,14 +1,62 @@
-import React, {Component} from 'react';
+import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
 
-export default class NewUser extends Component {
-
-    render() {
-        return (
-            <>
-                <span>##User Image##</span>
-                <span>New User</span>
-            </>
-        );
+const useStyles = makeStyles((theme) => ({
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    paper: {
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
     }
+}));
+
+export default function NewUser() {
+
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <>
+            <span>##User Image##</span>
+            <button onClick={handleOpen}>New User</button>
+            <Modal
+                aria-labelledby="transition-modal-title"
+                className={classes.modal}
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{timeout: 500}}
+            >
+                <Fade in={open}>
+                    <div className={classes.paper}>
+                        <h2 id="transition-modal-title">Create User</h2>
+                        <div><input type="text"/></div>
+                        <div><input type="text"/></div>
+                        <div><input type="text"/></div>
+                        <div>
+                            <button>Cancel</button>
+                            <button>Create User</button>
+                        </div>
+                    </div>
+                </Fade>
+            </Modal>
+        </>
+    );
 
 }
