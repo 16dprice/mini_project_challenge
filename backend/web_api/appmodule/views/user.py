40d9a8fd4392e1956a81caab625483a3b0635e76
AnonymousWalker <hoanganhtran1998@gmail.com>
2021-01-11 16:37:24 -0500

    add safety code to user view

--- backend/web_api/appmodule/views/user.py
+++ backend/web_api/appmodule/views/user.py
@@ -13,38 +13,50 @@ class UserList(APIView):
         return Response(serializerObject.data)
 
     def post(self, request):
-        username = request.data['username']
-        firstName = request.data['firstName']
-        lastName = request.data['lastName']
+        username = request.data.get('username', None)
+        firstName = request.data.get('firstName', None)
+        lastName = request.data.get('lastName', None)
 
-        User.objects.create(
-            username=username, first_name=firstName, last_name=lastName
-        )
-        return Response(status=status_code.HTTP_200_OK)
-
+        if username != None and firstName != None and lastName != None:
+                
+            User.objects.create(
+                username=username, first_name=firstName, last_name=lastName
+            )
+            return Response(status=status_code.HTTP_200_OK)
+        else:
+            return Response(status=status_code.HTTP_400_BAD_REQUEST)
 
 class UserInfo(APIView):
 
     def get(self, request, id):
-        user = User.objects.get(id=id)
-        serializerObject = UserSerializer(user)
-        return Response(serializerObject.data)
+        try:
+            user = User.objects.get(id=id)
+            serializerObject = UserSerializer(user)
+            return Response(serializerObject.data)
+        except:
+            return Response(status=status_code.HTTP_404_NOT_FOUND)
 
     def patch(self, request, id):
         user = User.objects.get(id=id)
-        firstName = request.data['firstName']
-        lastName = request.data['lastName']
+        firstName = request.data.get('firstName', None)
+        lastName = request.data('lastName', None)
 
-        user.first_name = firstName
-        user.last_name = lastName
-        user.save()
-
-        return Response(status=status_code.HTTP_200_OK)
+        if firstName != None and lastName != None:
+            user.first_name = firstName
+            user.last_name = lastName
+            user.save()
+            
+            return Response(status=status_code.HTTP_200_OK)
+        else: 
+            return Response(status=status_code.HTTP_400_BAD_REQUEST)
 
 
 class UserProject(APIView):
     def get(self, request, id):
-        user = User.objects.get(id=id)
-        projects = user.project_set.all()
-        serializerObject = ProjectSerializer(projects, many=True)
-        return Response(serializerObject.data)
+        try:
+            user = User.objects.get(id=id)
+            projects = user.project_set.all()
+            serializerObject = ProjectSerializer(projects, many=True)
+            return Response(serializerObject.data)
+        except:
+            return Response(status=status_code.HTTP_400_BAD_REQUEST)
