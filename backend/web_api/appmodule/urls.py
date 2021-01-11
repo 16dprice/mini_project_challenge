b57a6183a4ea59976b7018f63560f9e1edd20f73
AnonymousWalker <hoanganhtran1998@gmail.com>
2021-01-11 16:02:37 -0500

    prefix url regex with ^

--- backend/web_api/appmodule/urls.py
+++ backend/web_api/appmodule/urls.py
@@ -3,14 +3,14 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'projects/?$', views.projects),
-    url(r'projects/?(\?completed=((true)|(false)))', views.projects),
-    url(r'projects/(?P<id>\d+)/?$', views.project_detail),
-    url(r'projects/(?P<projectId>\d+)/contributors/?$', views.project_contributors),
-    url(r'projects/(?P<projectId>\d+)/available-users/?$', views.get_users_available_to_project),
-    url(r'users/?$', views.users),
-    url(r'users/(?P<id>\d+)/?$', views.user_info),
-    url(r'users/(?P<id>\d+)/projects/?$', views.user_projects),
-    url(r'languages/$', views.languages),
-    url(r'books/$', views.books),
+    url(r'^projects/?$', views.ProjectList.as_view()),
+    url(r'^projects/?(\?completed=((true)|(false)))', views.ProjectList.as_view()),
+    url(r'^projects/(?P<id>\d+)/?$', views.ProjectDetail.as_view()),
+    url(r'^projects/(?P<projectId>\d+)/contributors/?$', views.ProjectContributors.as_view()),
+    url(r'^projects/(?P<projectId>\d+)/available-users/?$', views.get_users_available_to_project),
+    url(r'^users/?$', views.UserList.as_view()),
+    url(r'^users/(?P<id>\d+)/?$', views.UserInfo.as_view()),
+    url(r'^users/(?P<id>\d+)/projects/?$', views.UserProject.as_view()),
+    url(r'^languages/$', views.languages),
+    url(r'^books/$', views.books),
 ]
