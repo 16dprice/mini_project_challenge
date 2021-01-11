b1c5b78c334f3e90b109bf15ae97c4a1dbc08e76
AnonymousWalker <hoanganhtran1998@gmail.com>
2021-01-11 10:56:51 -0500

    update url regex for filter projects

--- backend/web_api/appmodule/urls.py
+++ backend/web_api/appmodule/urls.py
@@ -3,8 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'projects/?', views.projects),
+    url(r'projects/?$', views.projects),
+    url(r'projects/?(\?completed=((true)|(false)))', views.projects),
     url(r'projects/(?P<id>\d+)/?$', views.project_detail),
     url(r'projects/(?P<projectId>\d+)/contributors/?$', views.project_contributors),
-    url(r'users/?$', views.users)
+    url(r'users/?$', views.users),
 ]
