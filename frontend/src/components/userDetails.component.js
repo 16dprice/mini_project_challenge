0cd75f1105b5d635bfd546312c6a88bf9e550cb8
DJ <djpric91@gmail.com>
2021-01-11 14:12:44 -0600

    add functionality to user details page to edit user

--- frontend/src/components/userDetails.component.js
+++ frontend/src/components/userDetails.component.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 
 import UserApiAdapter from "../api/userApiAdapter";
 import ProjectApiAdapter from "../api/projectApiAdapter";
+import {Button} from "@material-ui/core";
 
 export default class UserDetails extends Component {
 
@@ -10,26 +11,65 @@ export default class UserDetails extends Component {
 
         this.handleFirstNameChange = this.handleFirstNameChange.bind(this);
         this.handleLastNameChange = this.handleLastNameChange.bind(this);
+        this.updateUser = this.updateUser.bind(this);
 
         const user = UserApiAdapter.getUserById(props.match.params.userId);
         const projectsContributedTo = ProjectApiAdapter.getProjectsByUserId(user.id);
 
         this.state = {
             user,
-            projectsContributedTo
+            projectsContributedTo,
+            editingUser: false,
+            hasEditedUser: false
         };
     }
 
     handleFirstNameChange(e) {
         this.setState({
-            user: {...this.state.user, firstName: e.target.value}
+            user: {...this.state.user, firstName: e.target.value},
+            hasEditedUser: true
         });
     }
 
     handleLastNameChange(e) {
         this.setState({
-            user: {...this.state.user, lastName: e.target.value}
+            user: {...this.state.user, lastName: e.target.value},
+            hasEditedUser: true
         });
+    }
+
+    updateUser(e) {
+        if(this.state.hasEditedUser) {
+            // TODO: make POST request to update user here
+            console.log(this.state.user);
+        }
+
+        this.setState({
+            editingUser: false,
+            hasEditedUser: false
+        });
+    }
+
+    getFirstAndLastNamesInputs() {
+        if (this.state.editingUser) {
+            return <>
+                <div><input type="text" value={this.state.user.firstName} onChange={this.handleFirstNameChange}/></div>
+                <div><input type="text" value={this.state.user.lastName} onChange={this.handleLastNameChange}/></div>
+            </>
+        } else {
+            return <>
+                <div>{this.state.user.firstName}</div>
+                <div>{this.state.user.lastName}</div>
+            </>
+        }
+    }
+
+    getEditOrSaveButton() {
+        if(this.state.editingUser) {
+            return <Button onClick={this.updateUser} disabled={!this.state.hasEditedUser}>Save</Button>
+        } else {
+            return <Button onClick={() => this.setState({editingUser: true})}>Edit</Button>
+        }
     }
 
     getProjectsContributedTo() {
@@ -45,13 +85,8 @@ export default class UserDetails extends Component {
                 <div className="user-details">
                     <div className="user-details__names">
                         <span className="user-details__username">{this.state.user.username}</span>
-                        <span className="user-details__first-and-last-names">
-                            <div><input type="text" readOnly={false} value={this.state.user.firstName}
-                                    onChange={this.handleFirstNameChange}/></div>
-                            <div><input type="text" readOnly={false} value={this.state.user.lastName}
-                                    onChange={this.handleLastNameChange}/></div>
-                        </span>
-                        <button>Edit</button>
+                        <span className="user-details__first-and-last-names">{this.getFirstAndLastNamesInputs()}</span>
+                        {this.getEditOrSaveButton()}
                     </div>
                     <div className="user-details__projects">
                         {this.getProjectsContributedTo()}
