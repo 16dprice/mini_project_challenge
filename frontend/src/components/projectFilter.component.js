1a2a09170f6083744d539a0a53a5384dd2fc354f
DJ <djpric91@gmail.com>
2021-01-14 13:02:54 -0600

    show which value is checked on project filter on load

--- frontend/src/components/projectFilter.component.js
+++ frontend/src/components/projectFilter.component.js
@@ -6,20 +6,24 @@ import {all, completed, uncompleted} from "../actions/projectStatusActions";
 class ProjectFilter extends Component {
 
     render() {
+        const showingAll = this.props.status === 'all';
+        const showingCompleted = this.props.status === 'completed';
+        const showingUncompleted = this.props.status === 'uncompleted';
+
         return (
             <span className="project-filter">
                 <input type="radio" id="statusChoice1" name="status" value="all"
-                    onClick={() => this.props.all()}
+                    onClick={() => this.props.all()} checked={showingAll}
                 />
                 <label htmlFor="statusChoice1">all</label>
 
                 <input type="radio" id="statusChoice2" name="status" value="completed"
-                    onClick={() => this.props.completed()}
+                    onClick={() => this.props.completed()} checked={showingCompleted}
                 />
                 <label htmlFor="statusChoice2">completed</label>
 
                 <input type="radio" id="statusChoice3" name="status" value="uncompleted"
-                    onClick={() => this.props.uncompleted()}
+                    onClick={() => this.props.uncompleted()} checked={showingUncompleted}
                 />
                 <label htmlFor="statusChoice3">uncompleted</label>
             </span>
@@ -29,6 +33,6 @@ class ProjectFilter extends Component {
 }
 
 export default connect(
-    null,
+    state => ({ status: state.projectStatus }),
     {all, completed, uncompleted}
 )(ProjectFilter);
